9007fe61075a4779f3aecab776e59cb228891cc8
Hakan Akgül <hakanakgul34@gmail.com>
2022-11-22 11:04:55 +0300

    fontawesome included

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
+
+mix.disableNotifications();

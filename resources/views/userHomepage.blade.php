cf802ba1150289a42367743875c69599f2b6ff9a
Hakan Akgül <hakanakgul34@gmail.com>
2022-11-22 13:22:40 +0300

    added logout button

--- resources/views/userHomepage.blade.php
+++ resources/views/userHomepage.blade.php
@@ -3,6 +3,14 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __(Auth::user()->name . "'s Dashboard") }}
         </h2>
+        <!-- Authentication -->
+        <form class="absolute right-0 top-0 mt-4 " method="POST" action="{{ route('logout') }}" x-data>
+            @csrf
+
+            <x-jet-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                {{ __('Log Out') }}
+            </x-jet-dropdown-link>
+        </form>
     </x-slot>
 
     <div class="py-12">
